1ca68d6c85d140a26165db58adb84ba7d1defcb4
ILKOV <il-kow@mail.ru>
2019-04-02 16:21:11 +0400

    del(remote.repo)

--- git_remote.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from git import Repo, repo
-
-empty_repo = Repo.init(os.path.join('emptyrepo', 'empty'))
-origin = empty_repo.create_remote('origin', repo.remotes.origin.url)
-
-print(origin)
