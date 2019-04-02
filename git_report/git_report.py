73882c0b4d5f509e0051fdc77ae0c1923bb2953d
ILKOV <il-kow@mail.ru>
2019-04-02 15:41:39 +0400

    feat(Добавлен пустой файл для заполнения настройками для отправки почты, и т.д.)

--- git_report/git_report.py
+++ git_report/git_report.py
@@ -6,35 +6,40 @@ from datetime import datetime, timedelta
 from git_report.email_sender import send_mail
 
 
+# Make report from different repos
 def git_report():
     repos = json.load(open('./git_report/repos.json'))
 
     f = open("git_report/daily_report.txt", "w")
 
     for single_repo in repos:
-
-        print(single_repo['name'])
-        f.write(str(single_repo['name']))
-        f.write(str("\n"))
-
-        repo = Repo(single_repo['path'])
-
-        commits = list(repo.iter_commits())
-
-        for commit in commits:
-
-            ts = int(commit.committed_date)
-            commit_date = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d')
-
-            d = datetime.today() - timedelta(days=1)
-
-            if commit_date == d.strftime("%Y-%m-%d"):
-                print(commit.author, commit.message)
-
-                f.write(str(commit.message))
-
-        f.write(str("\n"))
+        get_repo_commits(single_repo, f)
 
     f.close()
 
     send_mail()
+
+
+# Get commits for one day of single repo
+def get_repo_commits(single_repo, f):
+    print(single_repo['name'])
+    f.write(str(single_repo['name']))
+    f.write(str("\n"))
+
+    repo = Repo(single_repo['path'])
+
+    commits = list(repo.iter_commits())
+
+    for commit in commits:
+
+        ts = int(commit.committed_date)
+        commit_date = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d')
+
+        d = datetime.today() - timedelta(days=1)
+
+        if commit_date == d.strftime("%Y-%m-%d"):
+            print(commit.author, commit.message)
+
+            f.write(str(commit.message))
+
+    f.write(str("\n"))
---
+++ settings/example.settings.py
@@ -0,0 +1,19 @@
+# SQL settings
+host = ''
+user = ''
+passwd = ''
+db = ''
+port = 0000
+
+# SMTP Mail settings
+smtp_server = 'smtp.gmail.com'
+mail_login = ''
+mail_passwd = ''
+
+# Email user report
+receiver = ['']
+cc = ['']
+
+# GIT report
+git_receiver = ['']
+git_cc = ['']
