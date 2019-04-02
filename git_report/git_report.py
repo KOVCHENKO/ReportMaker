897f2850a9b8a076345998440554c90a3b353e48
ILKOV <il-kow@mail.ru>
2019-04-02 14:03:04 +0400

    feat(Настройка отправки отчета по email. Тестирование отправки по email)

--- git_report/git_report.py
+++ git_report/git_report.py
@@ -1,38 +1,72 @@
 import json
+import os
+from email import encoders
+from email.mime.base import MIMEBase
 
+from smtplib import SMTP_SSL
+from email.mime.multipart import MIMEMultipart
 from git import Repo
 from datetime import datetime, timedelta
 
-repos = json.load(open('./git_report/repos.json'))
+from settings import mail_login, git_receiver, git_cc, smtp_server, mail_passwd
 
-f = open("git_report.txt", "w")
 
-for single_repo in repos:
+def main():
 
-    print(single_repo['name'])
-    f.write(str(single_repo['name']))
-    f.write(str("\n"))
+    repos = json.load(open('./git_report/repos.json'))
 
-    repo = Repo(single_repo['path'])
-    now = datetime.now()
+    f = open("git_report.txt", "w")
 
-    commits = list(repo.iter_commits())
+    for single_repo in repos:
 
-    for commit in commits:
+        print(single_repo['name'])
+        f.write(str(single_repo['name']))
+        f.write(str("\n"))
 
-        ts = int(commit.committed_date)
-        commit_date = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d')
-        today = now.strftime("%Y-%m-%d")
+        repo = Repo(single_repo['path'])
+        now = datetime.now()
 
-        d = datetime.today() - timedelta(days=1)
+        commits = list(repo.iter_commits())
 
-        if commit_date == d.strftime("%Y-%m-%d"):
-            print(commit.author, commit.message)
+        for commit in commits:
 
-            f.write(str(commit.message))
+            ts = int(commit.committed_date)
+            commit_date = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d')
+            today = now.strftime("%Y-%m-%d")
 
-    f.write(str("\n"))
+            d = datetime.today() - timedelta(days=1)
 
-f.close()
+            if commit_date == d.strftime("%Y-%m-%d"):
+                print(commit.author, commit.message)
 
+                f.write(str(commit.message))
+
+        f.write(str("\n"))
+
+    f.close()
+
+    # Compose attachment
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open("git_report.txt", "rb").read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename("git_report.txt"))
+
+    # Compose message
+    msg = MIMEMultipart()
+    msg['From'] = mail_login
+    msg['To'] = ', '.join(git_receiver)
+    msg['Cc'] = ', '.join(git_cc)
+    msg['Subject'] = 'Отчет по работе отдела за ' + str(datetime.today().strftime('%Y-%m-%d'))
+    msg.attach(part)
+
+    # Send mail
+    tosend = git_receiver + git_cc
+    smtp = SMTP_SSL('smtp.gmail.com')
+    smtp.connect(smtp_server)
+    smtp.login(mail_login, mail_passwd)
+    smtp.sendmail(mail_login, tosend, msg.as_string())
+    smtp.quit()
+
+if __name__ == '__main__':
+    main()
 
